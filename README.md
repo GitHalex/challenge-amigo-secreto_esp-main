d627e6884423061333f53d3c02ac12e8ea1f6e72
GitHalex <calex1731@gmail.com>
2025-09-23 11:59:29 -0400

    Agregando el proyecto completamente terminado

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Repositorio del Challenge Amigo Secretro Alura
+
+## este es aplicacion en donde se añade un amigo en una lista para luego sortear un amigo ganador entre los amigas agregados en la lista
+
+### añadiendo amigos
+
+---
+
+![Añadiendo amigos](img/photo_1.jpg)
+
+### amigo secreto
+
+---
+
+![Amigo Ganador](img/photo_2.jpg)
--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+// Función para agregar amigos a la lista
+function agregarAmigo() {
+  const inputAmigo = document.getElementById("amigo");
+  const nombreAmigo = inputAmigo.value.trim();
+
+  // Validar la entrada
+  if (nombreAmigo === "") alert("Por favor, inserte un nombre.");
+
+  // Actualizar el array de amigos
+  amigos.push(nombreAmigo);
+
+  // Limpiar el campo de entrada
+  inputAmigo.value = "";
+
+  // Actualizar la lista visual
+  actualizarListaAmigos();
+}
+
+// Función para actualizar la lista de amigos
+function actualizarListaAmigos() {
+  const listaAmigos = document.getElementById("listaAmigos");
+
+  // Limpiar la lista existente
+  listaAmigos.innerHTML = "";
+
+  // Iterar sobre el arreglo y crear elementos de lista
+  for (let i = 0; i < amigos.length; i++) {
+    const elementoLista = document.createElement("li");
+    elementoLista.textContent = amigos[i];
+    listaAmigos.appendChild(elementoLista);
+  }
+}
+
+// Función para sortear un amigo de forma aleatoria
+function sortearAmigo() {
+  if (amigos.length === 0)
+    alert("No hay amigos en la lista. Por favor, agregue al menos un amigo.");
+
+  // Generar un índice aleatorio => amigo ganador
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+  //nombre del amigo ganador en el arreglo
+  const amigoSorteado = amigos[indiceAleatorio];
+
+  const elementoResultado = document.getElementById("resultado");
+  elementoResultado.innerHTML = `<li>¡El amigo secreto es: ${amigoSorteado}!</li>`;
+
+  // eliminar la lista de amigos una vez que salga el ganador
+  amigos = [];
+  actualizarListaAmigos();
+}
+
+// Permitir agregar amigos presionando Enter
+document.getElementById("amigo").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    agregarAmigo();
+  }
+});
